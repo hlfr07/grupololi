4733b6cf000bcdc28079ccb8362b9b5262f3d326
hlfr07 <hf46078@gmail.com>
2024-02-18 15:02:10 -0500

    la fe x2

--- src/controllers/enviargmail.js
+++ src/controllers/enviargmail.js
@@ -1,49 +1,59 @@
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const jwt = require("jsonwebtoken");
+const axios = require("axios"); // Agrega esta línea para importar axios
 
 const enviarGMAIL = async (req, res) => {
-  const {nombre,apellido, correo, token } = req.body;
+  const { nombre, apellido, correo } = req.body;
 
-  const CLIENT_ID = "977106642985-tk77d9dc3sdi102kcanj8uq1ikhpib1s.apps.googleusercontent.com";
-  const CLIENT_SECRET = "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";
-  const REDIRECT_URL = "https://developers.google.com/oauthplayground";
-  const REFRESH_TOKEN =
-    "1//04Dd_gYvw9VInCgYIARAAGAQSNwF-L9Ir2qSo7r5rttbm1nnjOBGKT7aTXExQul2O55F6wHOflklDsNamsXPHUElV07C6FdNgaM8";
+  const usuario = "hector07nilas";  // Puedes cambiar esto dinámicamente si lo obtienes de algún lugar
+  const contrasena = "12345";  // Puedes cambiar esto dinámicamente si lo obtienes de algún lugar
 
-  const oAuth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URL
-  );
+  try {
+    const response = await axios.post("http://hoteles.spring.informaticapp.com:3093/empleados/authenticate", { usuario, contrasena });
 
-  oAuth2Client.setCredentials({
-    refresh_token: REFRESH_TOKEN,
-  });
+    const token = response.data;
+    console.log("Token recibido:", token);
 
-  async function sendEmail() {
-    try {
-      const accessToken = await oAuth2Client.getAccessToken();
-      const transporte = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          type: "OAuth2",
-          user: "hlfr46078@gmail.com",
-          clientId: CLIENT_ID,
-          clientSecret: CLIENT_SECRET,
-          refreshToken: REFRESH_TOKEN,
-          accessToken: accessToken,
-        },
-      });
+    const CLIENT_ID = "977106642985-tk77d9dc3sdi102kcanj8uq1ikhpib1s.apps.googleusercontent.com";
+    const CLIENT_SECRET = "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";
+    const REDIRECT_URL = "https://developers.google.com/oauthplayground";
+    const REFRESH_TOKEN =
+      "1//04Dd_gYvw9VInCgYIARAAGAQSNwF-L9Ir2qSo7r5rttbm1nnjOBGKT7aTXExQul2O55F6wHOflklDsNamsXPHUElV07C6FdNgaM8";
 
-      const resetLink = "http://localhost:4000/token"; // Reemplaza esto con la URL real de reseteo de contraseña
-/*
-      const payload = {
-        contra: "hotel",
-      };
+    const oAuth2Client = new google.auth.OAuth2(
+      CLIENT_ID,
+      CLIENT_SECRET,
+      REDIRECT_URL
+    );
 
-      const token = jwt.sign(payload, "hotel1801", { expiresIn: "1d" });*/
-      const contentHTML = `
+    oAuth2Client.setCredentials({
+      refresh_token: REFRESH_TOKEN,
+    });
+
+    async function sendEmail() {
+      try {
+        const accessToken = await oAuth2Client.getAccessToken();
+        const transporte = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            type: "OAuth2",
+            user: "hlfr46078@gmail.com",
+            clientId: CLIENT_ID,
+            clientSecret: CLIENT_SECRET,
+            refreshToken: REFRESH_TOKEN,
+            accessToken: accessToken,
+          },
+        });
+
+        const resetLink = "http://localhost:4000/token"; // Reemplaza esto con la URL real de reseteo de contraseña
+        /*
+              const payload = {
+                contra: "hotel",
+              };
+        
+              const token = jwt.sign(payload, "hotel1801", { expiresIn: "1d" });*/
+        const contentHTML = `
       <div style="max-width: 600px; margin: 50px auto; background-color:  #ffffff; border-radius: 25px; padding: 40px; box-shadow: 0 0 25px rgba(0, 0, 0, 0.2); text-align: center; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
       <h1 style="color: #ff6347; font-size: 36px; margin-bottom: 15px; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);">¡HOLA ${nombre} ${apellido}!</h1>
       <h2 style="color: #ff6347; font-size: 28px; margin-top: 15px; font-weight: bold;">Bienvenido al Increíble Sistema</h2>
@@ -55,27 +65,35 @@ const enviarGMAIL = async (req, res) => {
   </div>
   
 `;
-      const mailOptions = {
-        from: "hlfr46078@gmail.com",
-        to: correo,
-        subject: "TOKEN de autorización Hoteles",
-        html: contentHTML,
-      };
+        const mailOptions = {
+          from: "hlfr46078@gmail.com",
+          to: correo,
+          subject: "TOKEN de autorización Hoteles",
+          html: contentHTML,
+        };
 
-      const respuesta = await transporte.sendMail(mailOptions);
-      return respuesta;
+        const respuesta = await transporte.sendMail(mailOptions);
+        return respuesta;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    try {
+      const respuesta = await sendEmail();
+      res.status(200).send("Correo enviado");
     } catch (error) {
       console.log(error);
+      res.status(500).send("Error al enviar el correo");
     }
+
+  } catch (error) {
+    console.error("Error al hacer la solicitud POST:", error.message);
+    // Manejar el error apropiadamente
   }
 
-  try {
-    const respuesta = await sendEmail();
-    res.status(200).send("Correo enviado");
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Error al enviar el correo");
-  }
 };
 
 module.exports = enviarGMAIL;
+
+
